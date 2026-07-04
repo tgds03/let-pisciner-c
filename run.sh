0b0c2812e9b171dc0c009f5fcc05e9a196ae3e7d
Mingyu Kim <mingyuk2@c3r13s6.42gyeongsan.kr>
2026-07-04 17:17:26 +0900

    c00 & runner

--- run.sh
+++ run.sh
@@ -1,41 +1,47 @@
 #!/bin/bash
 
-USAGE_STR="usage: run.sh [repo_path] [stub_path]
+USAGE_STR="usage: $0 [repo_path] [stub_path]\n\
 Don't use it before commit all you need, or file may be deleted!"
 
 if [ "$#" -ne 2 ]; then
-	echo $USAGE_STR
+	echo -e $USAGE_STR
 	exit
 fi
 
 if [ ! -d "$1" ]; then
 	echo "can not find $1"
-	echo $USAGE_STR
+	echo -e $USAGE_STR
 	exit
 fi
 
 if [ ! -d "$2" ]; then
 	echo "can not find $2"
-	echo $USAGE_STR
+	echo -e $USAGE_STR
 	exit
 fi
 
+echo ""
 echo "Norminette: "
 norminette "$1" || exit
-echo ""
 
 for EXAM_DIR in $(cd "$2" && ls -d */); do
+	echo ""
 	echo -n "Put stubs in $EXAM_DIR: "
 	cp "$2/$EXAM_DIR/"* "$1/$EXAM_DIR/" || continue
 	echo "done"
+
 	echo -n "Compile and Run $EXAM_DIR: "
-	echo -ne $(cd "$1/$EXAM_DIR/" && cc -o app -Wall -Wextra -Werror *.c && ./app > output)
+	$(cd "$1/$EXAM_DIR/" && cc -o app -Wall -Wextra -Werror *.c && ./app > output) || continue
 	echo "done"
-	if [ -z $(diff "$2/$EXAM_DIR/expected_output" "$1/$EXAM_DIR/output") ]; then
+
+	if [ -z "$(diff "$2/$EXAM_DIR/expected_output" "$1/$EXAM_DIR/output")" ]; then
 		echo "$EXAM_DIR: Success"
+	else 
+		echo "$EXAM_DIR: Failed"
+		diff "$2/$EXAM_DIR/expected_output" "$1/$EXAM_DIR/output"
 	fi
-	echo ""
 done
 
+echo ""
 echo -n "Clean: "
-echo -ne $(cd "$1" && git clean -f && echo "done") 
+$(cd "$1" && git clean -qf) && echo "done" 
