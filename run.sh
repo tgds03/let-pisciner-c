d87644c9e0dde8f7bea06512194f49ab1b3a1b3c
Mingyu Kim <mingyuk2@c3r13s6.42gyeongsan.kr>
2026-07-04 17:01:17 +0900

    wip: runner

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-USAGE_STR='run.sh [repo_path] [stub_path]'
+USAGE_STR="usage: run.sh [repo_path] [stub_path]
+Don't use it before commit all you need, or file may be deleted!"
 
 if [ "$#" -ne 2 ]; then
 	echo $USAGE_STR
@@ -27,7 +28,14 @@ for EXAM_DIR in $(cd "$2" && ls -d */); do
 	echo -n "Put stubs in $EXAM_DIR: "
 	cp "$2/$EXAM_DIR/"* "$1/$EXAM_DIR/" || continue
 	echo "done"
-	echo "Compile: "
-	$(cd "$2/$EXAM_DIR/" && cc -o app -Wall -Wextra -Werror *.c && ./app)
+	echo -n "Compile and Run $EXAM_DIR: "
+	echo -ne $(cd "$1/$EXAM_DIR/" && cc -o app -Wall -Wextra -Werror *.c && ./app > output)
+	echo "done"
+	if [ -z $(diff "$2/$EXAM_DIR/expected_output" "$1/$EXAM_DIR/output") ]; then
+		echo "$EXAM_DIR: Success"
+	fi
 	echo ""
 done
+
+echo -n "Clean: "
+echo -ne $(cd "$1" && git clean -f && echo "done") 
