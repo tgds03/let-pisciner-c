25e0df01a405f15f912a1fb45bea4bcfcce1ff95
Mingyu Kim <mingyuk2@c3r14s1.42gyeongsan.kr>
2026-07-06 21:49:01 +0900

    refine c03/ex00

--- c03/ex00/stub.c
+++ c03/ex00/stub.c
@@ -8,10 +8,13 @@ int main() {
 	char *s3 = "55554";
 	char *s4 = "5555";
 	char *s5 = "555555";
+	char *s6 = "";
 	printf("%d ", ft_strcmp(s1, s1));
 	printf("%d ", ft_strcmp(s1, s2));
 	printf("%d ", ft_strcmp(s1, s3));
 	printf("%d ", ft_strcmp(s1, s4));
 	printf("%d ", ft_strcmp(s1, s5));
+	printf("%d ", ft_strcmp(s1, s6));
+	printf("%d ", ft_strcmp(s6, s6));
 	return 0;
 }
