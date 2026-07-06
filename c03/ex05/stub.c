7c23911ef644ebaa0934267d4d69e1c0bd79e7a7
tgds03 <xyzuct6800@naver.com>
2026-07-06 21:30:02 +0900

    c03/ex05 (strlcat)

--- c03/ex05/stub.c
+++ c03/ex05/stub.c
@@ -2,16 +2,16 @@
 #include <string.h>
 
 int main() {
-	char buffer[10] = "12345";
+	char buffer[11] = "12345";
 	char *str1 = "12";
 	char *str2 = "";
-	printf("%d %s\n", strlcat(buffer, str1, 10), buffer);
-	printf("%d %s\n", strlcat(buffer, str2, 10), buffer);
-	printf("%d %s\n", strlcat(buffer, str1, 10), buffer);
-	printf("%d %s\n", strlcat(buffer, str2, 10), buffer);
-	printf("%d %s\n", strlcat(buffer, str1, 10), buffer);
-	printf("%d %s\n", strlcat(buffer, str2, 10), buffer);
-	printf("%d %s\n", strlcat(buffer, str1, 10), buffer);
-	printf("%d %s\n", strlcat(buffer, str2, 10), buffer);
+	printf("%d %s\n", strlcat(buffer, str1, 11), buffer);
+	printf("%d %s\n", strlcat(buffer, str2, 11), buffer);
+	printf("%d %s\n", strlcat(buffer, str1, 11), buffer);
+	printf("%d %s\n", strlcat(buffer, str2, 11), buffer);
+	printf("%d %s\n", strlcat(buffer, str1, 11), buffer);
+	printf("%d %s\n", strlcat(buffer, str2, 11), buffer);
+	printf("%d %s\n", strlcat(buffer, str1, 11), buffer);
+	printf("%d %s\n", strlcat(buffer, str2, 11), buffer);
 	return 0;
 }
