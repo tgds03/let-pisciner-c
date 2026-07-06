6f2677b7b5b8119bf79ad69d2ee35eb30bbd7159
Mingyu Kim <mingyuk2@c3r14s1.42gyeongsan.kr>
2026-07-06 18:49:41 +0900

    c02

--- c02/ex06/stub.c
+++ c02/ex06/stub.c
@@ -3,7 +3,7 @@
 int ft_str_is_printable(char *str);
 
 int main() {
-	char c[2] = "\0\0";
+	char c[2] = "\0";
 	printf("%d", ft_str_is_printable(""));
 	for (int i = 0; i < 128; ++i) {
 		c[0] = (char)i;
