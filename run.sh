a8118604bb8a85c54ac1361581e0627d608b672f
Mingyu Kim <mingyuk2@c3r15s2.42gyeongsan.kr>
2026-07-07 20:07:13 +0900

    test on directory outside

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-USAGE_STR="usage: $0 [repo_path] [stub_path]\n\
+USAGE_STR="usage: $0 [repo_path] [exam_name]\n\
 Don't use it before commit all you need, or file may be deleted!"
 
 SHELL_PATH=$(cd "$(dirname "$0")" && pwd -P)
 export LIB_PATH="$SHELL_PATH/lib"
 export INCLUDE_PATH="$SHELL_PATH/include"
 export REPO_PATH="$1"
-export STUB_PATH="$2"
+export STUB_PATH="$SHELL_PATH/${2,,}"
 
 clean() {
 	echo ""
@@ -21,22 +21,22 @@ if [ "$#" -ne 2 ]; then
 fi
 
 if [ ! -d "$REPO_PATH" ]; then
-	echo "can not find $1"
+	echo "can not find $REPO_PATH"
 	echo -e $USAGE_STR
 	exit
 fi
 
 if [ ! -d "$STUB_PATH" ]; then
-	echo "can not find $2"
+	echo "can not find exam $2"
 	echo -e $USAGE_STR
 	exit
 fi
 
 echo ""
 echo "Norminette: "
-norminette "$1" || exit
+norminette "$REPO_PATH" || exit
 
-for EXAM_NAME in $(cd "$2" && ls -d */); do
-	EXAM_DIR="$EXAM_NAME" ./cmd/compile_exam.sh || continue
-	EXAM_DIR="$EXAM_NAME" ./cmd/test_exam.sh
+for EXAM_NAME in $(cd "$SUTB_PATH" && ls -d */); do
+	EXAM_DIR="$EXAM_NAME" "$SHELL_PATH/cmd/compile_exam.sh" || continue
+	EXAM_DIR="$EXAM_NAME" "$SHELL_PATH/cmd/test_exam.sh"
 done
